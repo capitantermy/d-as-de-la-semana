50c0398cbf78b9ac0ea3f63b364b0f286cb259e5
josé <55531215+capitantermy@users.noreply.github.com>
2019-10-25 08:40:24 +0100

    Update dias semana.py

--- dias semana.py
+++ dias semana.py
@@ -1,24 +1,24 @@
 def dias_de_la_semana():
-    print 'Escribe un número del 1 al 7' 
+    print 'Escribe un nÃºmero del 1 al 7' 
     print 'para saber a que dia de la semana corresponde'
     repetir=True
     while (repetir==True):
-        dia=input('Pon el número de día:')
-        repetir = (dia<0) or (dia>7)
+        dia=input('Pon el nÃºmero de dÃ­a:')
+        repetir = (dia<1) or (dia>7)
         if (repetir==True):
-            print 'Repetir número'
+            print 'Repetir nÃºmero'
     if(dia==1):
         print'lunes'
     if(dia==2):
         print'martes'
     if(dia==3):
-        print'miércoles'
+        print'miÃ©rcoles'
     if(dia==4):
         print'jueves'
     if(dia==5):
         print'viernes'
     if(dia==6):
-        print'sábado'
+        print'sÃ¡bado'
     if(dia==7):
         print'domingo'
 dias_de_la_semana()
